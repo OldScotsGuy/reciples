4f76d0c6bc318ecd2e5cecf6d1de8d7c8fede479
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-06-16 12:37:15 +0100

    Completed index controller stub

--- src/main/java/org/nickharle/recipeapp/controllers/IndexController.java
+++ src/main/java/org/nickharle/recipeapp/controllers/IndexController.java
@@ -1,4 +1,14 @@
 package org.nickharle.recipeapp.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String getIndexPage() {
+        return "index";
+    }
+
 }
