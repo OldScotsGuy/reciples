71eab7f3f07b3bb261f5b891df2bbf237a3ec8db
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-06-22 22:26:42 +0100

    Setup queries on the Categories and UnitOfMeasure repositories

--- src/main/java/org/nickharle/recipeapp/repositories/CategoryRepository.java
+++ src/main/java/org/nickharle/recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,10 @@ package org.nickharle.recipeapp.repositories;
 import org.nickharle.recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
+
 }
--- src/main/java/org/nickharle/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/org/nickharle/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,10 @@ package org.nickharle.recipeapp.repositories;
 import org.nickharle.recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
+
 }
