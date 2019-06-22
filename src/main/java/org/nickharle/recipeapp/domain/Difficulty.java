14c1b6d4d08e4ccf2040b0d7770c48ac0285f5b6
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-06-22 20:58:49 +0100

    Added in Difficulty enum

---
+++ src/main/java/org/nickharle/recipeapp/domain/Difficulty.java
@@ -0,0 +1,7 @@
+package org.nickharle.recipeapp.domain;
+
+public enum Difficulty {
+
+    EASY, MODERATE, KIND_OF_HARD, HARD
+
+}
--- src/main/java/org/nickharle/recipeapp/domain/Recipe.java
+++ src/main/java/org/nickharle/recipeapp/domain/Recipe.java
@@ -17,8 +17,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // todo add difficulty
-    //private Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @Lob
     private Byte[] image;
@@ -91,6 +92,14 @@ public class Recipe {
 
     public void setDirections(String directions) {
         this.directions = directions;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 
     public Byte[] getImage() {
