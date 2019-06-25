829f22df76a3ec382fbf7ee46046b18c0ddb62d9
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-06-25 19:22:55 +0100

    Removed bidirectional references from equals and hash in Project Lombok

--- src/main/java/org/nickharle/recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/org/nickharle/recipeapp/bootstrap/RecipeBootstrap.java
@@ -8,6 +8,7 @@ import org.nickharle.recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -29,6 +30,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         recipeRepository.saveAll(getRecipes());
         log.debug("Bootstrap data loaded");
