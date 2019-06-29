debea901d8127fbf190616665594315dcba43aac
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-06-29 11:51:39 +0100

    More JUnit tests added

---
+++ src/test/java/org/nickharle/recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,34 @@
+package org.nickharle.recipeapp.repositories;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.nickharle.recipeapp.domain.UnitOfMeasure;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)    // Starts Spring Context
+@DataJpaTest                   // Brings up an embedded database
+public class UnitOfMeasureRepositoryIT {
+
+    @Autowired                  // Can use @Autowird as the context will start
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByDescription() throws Exception {
+
+        Optional <UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", uomOptional.get().getDescription());
+
+    }
+}
