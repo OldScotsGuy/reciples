ee87386b80772f3e33179a2cf1a1a12fe2427755
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-07-03 22:07:16 +0100

    Create recipe from partially implemented

--- src/main/java/org/nickharle/recipeapp/controllers/RecipeController.java
+++ src/main/java/org/nickharle/recipeapp/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package org.nickharle.recipeapp.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.nickharle.recipeapp.commands.RecipeCommand;
 import org.nickharle.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -26,4 +29,19 @@ public class RecipeController {
         return "recipe/show";
     }
 
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    // @RequestMapping(name="recipe", method = RequestMethod.POST) --- Old way
+    @PostMapping
+    @RequestMapping("recipe")
+    // @ModelAttribute tells Spring to bind the form parameters to the RecipeCommand object
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show" + savedCommand.getId();
+    }
 }
