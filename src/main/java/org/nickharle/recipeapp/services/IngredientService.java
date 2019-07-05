dee5d1c5b525419e544502cfdad5659b60b30674
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-07-05 21:23:11 +0100

    Show ingredient from recipe implemented

---
+++ src/main/java/org/nickharle/recipeapp/services/IngredientService.java
@@ -0,0 +1,9 @@
+package org.nickharle.recipeapp.services;
+
+import org.nickharle.recipeapp.commands.IngredientCommand;
+
+public interface IngredientService {
+
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+
+}
