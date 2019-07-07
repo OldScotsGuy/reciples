cd998d4f02666b25925d1e23bf929549f08f8c47
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-07-07 13:13:17 +0100

    Update ingredient from recipe implemented

---
+++ src/main/java/org/nickharle/recipeapp/services/UnitOfMeasureServiceImpl.java
@@ -0,0 +1,31 @@
+package org.nickharle.recipeapp.services;
+
+import org.nickharle.recipeapp.commands.UnitOfMeasureCommand;
+import org.nickharle.recipeapp.converters.UnitOfMeasureToUnitOfMeasureCommand;
+import org.nickharle.recipeapp.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class UnitOfMeasureServiceImpl implements UnitOfMeasureService {
+
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+    private final UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand;
+
+    public UnitOfMeasureServiceImpl(UnitOfMeasureRepository unitOfMeasureRepository, UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand) {
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+        this.unitOfMeasureToUnitOfMeasureCommand = unitOfMeasureToUnitOfMeasureCommand;
+    }
+
+    @Override
+    public Set<UnitOfMeasureCommand> listAllUoms() {
+
+        return StreamSupport.stream(unitOfMeasureRepository.findAll()
+                .spliterator(), false)
+                .map(unitOfMeasureToUnitOfMeasureCommand::convert)
+                .collect(Collectors.toSet());
+    }
+}
--- src/test/java/org/nickharle/recipeapp/services/IngredientServiceImplementationTest.java
+++ src/test/java/org/nickharle/recipeapp/services/IngredientServiceImplementationTest.java
@@ -5,15 +5,19 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.nickharle.recipeapp.commands.IngredientCommand;
+import org.nickharle.recipeapp.converters.IngredientCommandToIngredient;
 import org.nickharle.recipeapp.converters.IngredientToIngredientCommand;
+import org.nickharle.recipeapp.converters.UnitOfMeasureCommandToUnitOfMeasure;
 import org.nickharle.recipeapp.converters.UnitOfMeasureToUnitOfMeasureCommand;
 import org.nickharle.recipeapp.domain.Ingredient;
 import org.nickharle.recipeapp.domain.Recipe;
 import org.nickharle.recipeapp.repositories.RecipeRepository;
+import org.nickharle.recipeapp.repositories.UnitOfMeasureRepository;
 
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -22,22 +26,30 @@ import static org.mockito.internal.verification.VerificationModeFactory.times;
 public class IngredientServiceImplementationTest {
 
     private final IngredientToIngredientCommand ingredientToIngredientCommand;
+    private final IngredientCommandToIngredient ingredientCommandToIngredient;
 
     @Mock
     RecipeRepository recipeRepository;
+
+    @Mock
+    UnitOfMeasureRepository unitOfMeasureRepository;
 
     IngredientService ingredientService;
 
     //init converters
     public IngredientServiceImplementationTest() {
         this.ingredientToIngredientCommand = new IngredientToIngredientCommand(new UnitOfMeasureToUnitOfMeasureCommand());
+        this.ingredientCommandToIngredient = new IngredientCommandToIngredient(new UnitOfMeasureCommandToUnitOfMeasure());
     }
 
     @Before
     public void setUp() throws Exception {
 
         MockitoAnnotations.initMocks(this);
-        ingredientService = new IngredientServiceImplementation(ingredientToIngredientCommand, recipeRepository);
+        ingredientService = new IngredientServiceImplementation(ingredientToIngredientCommand,
+                                                                ingredientCommandToIngredient,
+                                                                recipeRepository,
+                                                                unitOfMeasureRepository);
     }
 
     @Test
@@ -70,4 +82,31 @@ public class IngredientServiceImplementationTest {
         assertEquals(Long.valueOf(1L), ingredientCommand.getRecipeId());
         verify(recipeRepository, times(1)).findById(anyLong());
     }
+
+    @Test
+    public void testSaveIngredientCommand() throws Exception {
+        //given
+        IngredientCommand command = new IngredientCommand();
+        command.setId(3L);
+        command.setRecipeId(2L);
+
+        Optional<Recipe> recipeOptional = Optional.of(new Recipe());
+
+        Recipe savedRecipe = new Recipe();
+        savedRecipe.addIngredient(new Ingredient());
+        savedRecipe.getIngredients().iterator().next().setId(3L);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+        when(recipeRepository.save(any())).thenReturn(savedRecipe);
+
+        //when
+        IngredientCommand savedCommand = ingredientService.saveIngredientCommand(command);
+
+        //then
+        assertEquals(Long.valueOf(3L), savedCommand.getId());
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, times(1)).save(any(Recipe.class));
+
+    }
+
 }
